2b1b5982b06cee78c0cd5ca3a4a5e66232de6945
faheemamu <faheem.fintech@gmail.com>
2024-03-21 17:44:26 +0530

    inside main

--- src/main/java/com/referal/referApi/ReferApiApplication.java
+++ src/main/java/com/referal/referApi/ReferApiApplication.java
@@ -1,7 +1,9 @@
 package com.referal.referApi;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.StaticApplicationContext;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -10,9 +12,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class ReferApiApplication {
 
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ReferApiApplication.class, args);
 		
+System.out.println("inside main method of the application");
 		
 		StringBuilder sb1= new StringBuilder("faheem");
 	String	str1 = sb1.toString();
